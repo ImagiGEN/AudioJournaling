8bb513459d82f4fd5ec83735a87d2a61b571a5d4
Ashritha Goramane <ashritha.goramane@gmail.com>
2023-08-04 15:14:18 -0400

    Updated test

--- tests/pytest/default.py
+++ tests/pytest/default.py
@@ -1,25 +1,20 @@
 import unittest
-import os
-import pathlib
-import bcrypt
-from utils.common import read_yaml, validate_username, get_hashed_password
 
+class TestDefault(unittest.TestCase):
 
-class TestCommonUtils(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_read_yaml(self):
-        self.assertEqual(read_yaml(os.path.join(pathlib.Path().resolve(
-        ), "tests/test.yaml")), {"UNIT_TESTING": {"DUMMY": "dummy_value"}})
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
 
-    def test_validate_username(self):
-        self.assertTrue(validate_username("jane.doe@gmail.com"))
-        self.assertTrue(validate_username("jane.doe@gmail.co.uk"))
-        self.assertFalse(validate_username("jane.doe"))
-
-    def test_hashedPassword(self):
-        password = "$ecret@123"
-        hashed_pwd = get_hashed_password(password)
-        self.assertTrue(bcrypt.checkpw(password.encode('utf-8'), hashed_pwd))
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
