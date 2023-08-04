f79b0c245d4c10832ced3ec7ddb2b735d6e19314
Ashritha Goramane <ashritha.goramane@gmail.com>
2023-08-04 15:10:41 -0400

    Minor fixes

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -19,28 +19,5 @@ st.markdown(
     """
 )
 
-import yaml
-from yaml.loader import SafeLoader
-with open('credentials.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
-name, authentication_status, username = authenticator.login('Login', 'main')
-
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
-
 # Run the app
 # streamlit run main.py
