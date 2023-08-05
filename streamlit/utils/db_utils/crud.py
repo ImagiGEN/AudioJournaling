7fbec87186ebd5d85e9eace8fa9ac302e2d707b2
Ashritha Goramane <ashritha.goramane@gmail.com>
2023-08-05 16:29:26 -0400

    Added JWT token creation

--- streamlit/utils/db_utils/crud.py
+++ streamlit/utils/db_utils/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from utils.db_utils import models, schemas
+from utils import generic
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(username=user.username,
@@ -13,6 +14,18 @@ def create_user(db: Session, user: schemas.UserCreate):
 
 def authenticate_user(db: Session, credentials: schemas.UserAuthentication):
     result_user = db.query(models.User).filter(models.User.username == credentials.username).first()
-    if not result_user:
-        return False
-    return result_user.check_password(credentials.password)
+    if not result_user: # username doesn't exists
+        return
+    if result_user.check_password(credentials.password):
+        return generate_jwt_token(credentials.username, credentials.password)
+
+def generate_jwt_token(username, password):
+    if not (username and password):
+        raise Exception(
+            status_code=404, detail=r"Username and password cannot be empty")
+    data_to_encode = {
+        "username": username,
+        "password": generic.get_hashed_password(password).decode('utf-8')
+    }
+    access_token = generic.create_access_token(data_to_encode)
+    return access_token
--- streamlit/utils/generic.py
+++ streamlit/utils/generic.py
@@ -6,3 +6,19 @@ def get_hashed_password(password):
 
 def parse_timestamp(timestamp):
     return timestamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+from jose import jwt
+from datetime import datetime, timedelta
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY", "")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
