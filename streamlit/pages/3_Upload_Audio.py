a792440107f769b80f0133ce3f6d0db45e348332
Parvati Sohani <sohani.p@northeastern.edu>
2023-08-08 07:17:49 -0400

    changed path name

--- streamlit/pages/3_Upload_Audio.py
+++ streamlit/pages/3_Upload_Audio.py
@@ -3,6 +3,7 @@ import soundfile as sf
 import numpy as np
 import os
 import io
+from datetime import datetime
 from audio_recorder_streamlit import audio_recorder
 
 # Function to calculate audio duration
@@ -13,7 +14,8 @@ def get_audio_duration(audio_data):
 
 page_names = ['UploadAudio', 'RecordAudio'] 
 page = st.radio('Select one', page_names)
-st.write("return:", page)
+
+timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
 def upload_audio_page():
     st.write("This is the upload audio page.")
@@ -22,7 +24,7 @@ def upload_audio_page():
     st.title("Upload Audio")
     uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
 
-    if uploaded_file is not None:  # Check if a file has been uploaded
+    if uploaded_file is not None: 
         try:
             audio_data, sample_rate = sf.read(uploaded_file)
             audio_duration = get_audio_duration((audio_data, sample_rate))
@@ -31,7 +33,9 @@ def upload_audio_page():
 
             # Save the audio file to a local path if the "Submit Uploaded File" button is clicked
             if st.button("Submit Uploaded File"):
-                audio_path = f"uploaded_audio.{uploaded_file.name.split('.')[-1]}"
+                
+                #audio_path = f"uploaded_audio.{uploaded_file.name.split('.')[-1]}"
+                audio_path = f"uploaded_audio_{timestamp}.{uploaded_file.name.split('.')[-1]}"
                 with open(audio_path, 'wb') as f:
                     f.write(uploaded_file.read())
                 st.write(f"Audio file saved at: {audio_path}")
@@ -57,7 +61,7 @@ def upload_live_page():
             st.audio(audio_bytes, format="audio/wav")
             if st.button("Submit Live Audio"): 
             # Save the audio file to a local path
-             audio_path = "recorded_audio.wav"
+             audio_path = f"recorded_audio_{timestamp}.wav"
              sf.write(audio_path, audio_data, default_sample_rate)
 
              st.write(f"Audio file saved at: {audio_path}")
