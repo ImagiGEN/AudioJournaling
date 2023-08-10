5a3eda87b1a07698ad7fceeef3a58ef57807c090
Parvati Sohani <sohani.p@northeastern.edu>
2023-08-10 16:48:57 -0400

    Updated mood chart streamlit

--- streamlit/pages/5_Mood_Chart.py
+++ streamlit/pages/5_Mood_Chart.py
@@ -1,5 +1,31 @@
+import streamlit as st
+from utils import backend
 import plotly.express as px
-df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-fig = px.pie(df, values='pop', names='country', title='Population of European continent')
-fig.show()
+
+def plotly_chart():
+    data = get_user_emotions()
+    fig = px.pie(values=data.values(), names=data.keys(), hole=0.5, title='Mood chart')
+    return fig
+
+def get_user_emotions():
+    emotions = {
+        "sad": 1,
+        "happy": 3,
+        "disturbed": 4
+    }
+    return emotions
+
+
+st.title("Mood Chart")
+
+def authentication():
+    response = backend.validate_access_token(st.session_state.auth_token)
+    return response
+
+auth_user = authentication()
+
+if auth_user:
+    fig = plotly_chart()
+    st.plotly_chart(fig)
+else:
+    st.warning('Access Denied! Please authenticate yourself on User Authentication.', icon="⚠️")
