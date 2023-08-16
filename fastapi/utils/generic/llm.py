47e1ade1e8fb99bbada6dfc4e465d5ef6831fc94
Ashritha Goramane <44136484+ashrithagoramane@users.noreply.github.com>
2023-08-16 11:55:10 -0400

    Added updated prompts (#55)

--- fastapi/utils/generic/llm.py
+++ fastapi/utils/generic/llm.py
@@ -4,7 +4,9 @@ import os
 openaikey = os.getenv("OPENAI_API_KEY")
 
 def generate_suggestions(audio_transcript, emotion):
-    prompt = "Generate a 2 line positive quote for the with given context and emotion. Context:{} Emotion:{}".format(audio_transcript, emotion)
+    prompt = """I want you to act as a motivational friend and generate a 2 line positive quote for the with given context and emotion. 
+                Make it conversational to ensure that the mood is uplifted. 
+                Context:{} Emotion:{}""".format(audio_transcript, emotion)
     print(prompt)
     openai.api_key = openaikey
     promptstr=prompt
@@ -20,7 +22,16 @@ def generate_suggestions(audio_transcript, emotion):
     return response.get("choices")[0]["text"]
 
 def generate_summary(audio_transcript):
-    prompt = "Generate 200 words summary for the give personal journal. Journal:{}".format(audio_transcript)
+    prompt = """Can you provide a comprehensive summary of the given text? 
+    The summary should cover all the key points and main ideas presented in the original text, 
+    while also condensing the information into a concise and easy-to-understand format.
+    Give the summary from the person's point of view.
+    Please ensure that the summary includes relevant details and examples that support the main ideas, 
+    while avoiding any unnecessary information or repetition. 
+    The length of the summary should be appropriate for the length and complexity of the original text, 
+    providing a clear and accurate overview without omitting any important information and completing the meaningful sentences.
+    To ensure accuracy, please read the text carefully and pay attention to any nuances or complexities in the language.
+    Text:{}""".format(audio_transcript)
     print(prompt)
     openai.api_key = openaikey
     promptstr=prompt
